e8da64daa63f28d5817153589b5f20a24e7a3add
redcardinal989 <redcardinal989@gmail.com>
2026-01-12 09:46:17 -0500

    Second Commit
    added json and javascript. and linked my html to both css and javascript.

---
+++ Watchlist Project (sadique)/script.js
@@ -0,0 +1,40 @@
+console.log("js console");
+
+let data; 
+let grid = document.querySelector(".grid-container");
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    data = JSON.parse(xhttp.responseText);
+    console.log(data);
+   
+    data.forEach(function(show) {
+      let card = document.createElement("div");
+    
+      card.classList.add("card");
+
+      let textData=
+      "<div class='show-title'>"+show.title+"</div>"+
+      "<span>"+
+      "Publisher:" +show.publisher+"<br>"+
+        "Genre:" +show.genre+"<br>"+
+        "Seasons:" +show.seasons+"<br>"+
+        "Year:" +show.year+"<br>"+
+        "Rating:" +show.rating+"<br>"+
+      "</span>";
+
+      card.innerHTML = textData;
+     
+      if(show.imgSrc) {
+        card.style.backgroundImage = "url('"+show.imgSrc+"')";
+      }
+    
+        grid.appendChild(card);
+  });
+
+  }
+};
+xhttp.open("GET", "showdata.json", true);
+xhttp.send();
+
