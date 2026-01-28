4dd8bfebe1fc45b2246e9c57a50a098592a6bf9a
redcardinal989 <redcardinal989@gmail.com>
2026-01-28 09:37:05 -0500

    Eight Commit
    Debugged my form

--- Watchlist Project (sadique)/form.js
+++ Watchlist Project (sadique)/form.js
@@ -1,12 +1,12 @@
-const form = document.createElement('form');
-form.id = 'watchlistForm';
-document.body.appendChild(form);
+const form = document.querySelector('#watchlistForm');
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit",function(e){
+
+
     e.preventDefault();
     console.log('Form submitted:', {
         title: document.getElementById('title').value,
         rating: document.getElementById('rating').value,
         status: document.getElementById('status').value
     });
-});
+})
