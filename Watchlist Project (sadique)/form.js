ed9679cbab2c8da1e1b3181d79ba804c512982c4
redcardinal989 <redcardinal989@gmail.com>
2026-02-04 09:44:41 -0500

    Tenth commit
    changed up my grids, fixed my form

--- Watchlist Project (sadique)/form.js
+++ Watchlist Project (sadique)/form.js
@@ -2,23 +2,23 @@ const form = document.querySelector('#watchlistForm');
 
 form.addEventListener("submit",function(e){
     e.preventDefault();
-    // let title = titleInput.value;
-    // let publisher = publisherInput.value;
-    // let releaseDate = releaseDateInput.value;
-    // let rating = ratingInput.value;
-    // let status = statusInput.value;
-    // let newObj = {
-    //     "id" :getNextId(),
-    //     "title": title,
-    //     "publisher": publisher,
-    //     "releaseDate": releaseDate,
-    //     "rating": rating,
-    //     "status": status
-    // };
-    // sumbitDat(newObj);
-    // let existingData = JSON.parse(localStorage.getItem("watchlist"));// || [];
-    // existingData.push(newObj);
-    // localStorage.setItem("watchlist", JSON.stringify(existingData));
+    let title = titleInput.value;
+    let publisher = publisherInput.value;
+    let releaseDate = releaseDateInput.value;
+    let rating = ratingInput.value;
+    let status = statusInput.value;
+    let newObj = {
+        "id" :getNextId(),
+        "title": title,
+        "publisher": publisher,
+        "releaseDate": releaseDate,
+        "rating": rating,
+        "status": status
+    };
+    sumbitDat(newObj);
+    let existingData = JSON.parse(localStorage.getItem("watchlist"));// || [];
+    existingData.push(newObj);
+    localStorage.setItem("watchlist", JSON.stringify(existingData));
 
     console.log('Form submitted:', {
         title: document.getElementById('title').value,
